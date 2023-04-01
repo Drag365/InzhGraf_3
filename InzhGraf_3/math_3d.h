c1d993f906f6820587e454588d71c176b5be6e4b
Drag365 <ksilantev92@gmail.com>
2023-04-01 16:29:12 +0500

    Реализована привязка камеры к мышке

--- InzhGraf_3/math_3d.h
+++ InzhGraf_3/math_3d.h
@@ -4,8 +4,14 @@
 #include <stdio.h>
 #include <math.h>
 
-#define ToRadian(x) ((x) * 3.14 / 180.0f)
-#define ToDegree(x) ((x) * 180.0f / 3.14)
+#define ToRadian(x) ((x) * 3.14159265358979323846 / 180.0f)
+#define ToDegree(x) ((x) * 180.0f / 3.14159265358979323846)
+
+struct Vector2i
+{
+    int x;
+    int y;
+};
 
 struct Vector3f
 {
@@ -55,11 +61,23 @@ struct Vector3f
 
     Vector3f& Normalize();
 
+    void Rotate(float Angle, const Vector3f& Axis);
+
     void Print() const
     {
         printf("(%.02f, %.02f, %.02f", x, y, z);
     }
 };
+
+
+inline Vector3f operator+(const Vector3f& l, const Vector3f& r)
+{
+    Vector3f Ret(l.x + r.x,
+        l.y + r.y,
+        l.z + r.z);
+
+    return Ret;
+}
 
 inline Vector3f operator-(const Vector3f& l, const Vector3f& r)
 {
@@ -121,5 +139,20 @@ public:
     void InitPersProjTransform(float FOV, float Width, float Height, float zNear, float zFar);
 };
 
+
+struct Quaternion
+{
+    float x, y, z, w;
+
+    Quaternion(float _x, float _y, float _z, float _w);
+
+    void Normalize();
+
+    Quaternion Conjugate();
+};
+
+Quaternion operator*(const Quaternion& l, const Quaternion& r);
+
+Quaternion operator*(const Quaternion& q, const Vector3f& v);
 
 #endif	/* MATH_3D_H */
